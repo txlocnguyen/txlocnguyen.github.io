fe516af552b580b388403886db9ac45386142651
Loc Nguyen <txlocnguyen@gmail.com>
2022-01-27 21:37:27 -0600

    add typing animation

---
+++ assets/theme/js/typing.js
@@ -0,0 +1,12 @@
+const typed = new Typed(".typing", {
+    strings: ["Software Developer", "Software Engineer", "Full Stack Developer", "Full Stack Engineer", "Programmer", "Web Developer" ],
+    typeSpeed: 80,
+    backSpeed: 30,
+    loop: true
+});
+const typed2 = new Typed(".typing-hi", {
+    strings: ["Hello", "Hey there", "Greetings", "Howdy", "Hi there"],
+    typeSpeed: 200,
+    backSpeed: 100,
+    loop: true
+});
